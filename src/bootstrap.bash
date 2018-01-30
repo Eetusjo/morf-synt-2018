368da5081e03d686ddc8884252015a5e59b353b8
mathiascreutz <mathias.jp.creutz@gmail.com>
2018-01-30 14:19:21 +0200

    Fixed PYTHONPATH

--- src/bootstrap.bash
+++ src/bootstrap.bash
@@ -6,7 +6,7 @@ rm -r work
 
 export GIT_COMMITTER_NAME=anonymous
 export GIT_COMMITTER_EMAIL=anon@localhost
-export PYTHONPATH=/home/jovyan/src
+export PYTHONPATH=/home/jovyan/morf-synt-2018/src
 
 git clone https://github.com/Eetusjo/morf-synt-2018.git
 
