8e3fbf1f24e5e77d1549ce65b13d34a318a88ea4
mathiascreutz <mathias.jp.creutz@gmail.com>
2018-01-30 15:40:32 +0200

    Changed bootstrap script again

--- src/bootstrap.bash
+++ src/bootstrap.bash
@@ -8,5 +8,3 @@ export GIT_COMMITTER_NAME=anonymous
 export GIT_COMMITTER_EMAIL=anon@localhost
 
 git clone https://github.com/Eetusjo/morf-synt-2018.git
-
-source /home/jovyan/morf-synt-2018/src/environment.bash
--- src/environment.bash
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-export PYTHONPATH=/home/jovyan/morf-synt-2018/src
